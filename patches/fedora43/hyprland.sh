0e4e73a0988550a72540d00a8dda3cef29d0950e
Karboggy <karboggy@protonmail.com>
2026-08-21 14:51:35 +0200

    hyprland: patch substr() for fedora 43

--- patches/fedora43/hyprland.sh
+++ patches/fedora43/hyprland.sh
@@ -2,4 +2,8 @@ apply_fedora43_hyprland_patches() {
     if grep -q 'std::ranges::starts_with(str_view, prefixes)' src/helpers/MiscFunctions.cpp; then
         sed -i 's/std::ranges::starts_with(str_view, prefixes)/std::ranges::equal(str_view | std::views::take(prefixes.size()), prefixes)/' src/helpers/MiscFunctions.cpp
     fi
+
+    if [[ -f src/ipc/s1/S1.cpp ]] && grep -q 'request.subview(i, 2)' src/ipc/s1/S1.cpp; then
+        sed -i 's/request.subview(i, 2)/request.substr(i, 2)/' src/ipc/s1/S1.cpp
+    fi
 }
